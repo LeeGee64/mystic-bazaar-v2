fc6aca59d1a15ea54cc1b94b6efaf64ecbd29eff
LeeGee64 <writerlelandgill@gmail.com>
2022-12-06 18:42:10 -0500

    product page configuration

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,4 +1,4 @@
-import Layout from './components/layouts/layout'
+import Layout from '/components/layouts/layout'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import useSWR from 'swr';
@@ -6,12 +6,12 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Product() {
-    let apiUrl = '../api/staticdata';
+    let apiUrl = '/api/staticdata';
     const router = useRouter()
-    const { product } = router.query
+    const {product}  = router.query
   
     
-    const { data, error } = useSWR(product? apiUrl : null,fetcher);
+    const { data, error } = useSWR({product} ? apiUrl : null,fetcher);
     
         //Handle the error state
         if (error) return <div>Failed to load</div>;
@@ -21,17 +21,27 @@ export default function Product() {
         const productObject = JSON.parse(data);
         const prodArray = productObject.products;
 
-        const productItem = prodArray.find((p) => p.name === product);
+        const productItem = prodArray.find((p) => p.slug === product);
+       
 
         return (
-            <Layout>
-                <Image alt={productItem.name} 
-                        src={productItem.images[0].url} 
-                        placeholder= "/product-img-placeholder.svg" 
-                        width={100} 
-                        height={100}/>
-                <h1>{productItem.name}</h1>
-                <p>{}</p>
+            <Layout> {productItem ? <>
+                        <Image alt={productItem.name} 
+                                src={productItem.images[0].url} 
+                                placeholder= "/product-img-placeholder.svg" 
+                                width={100} 
+                                height={100}/>
+                        {/* Gallery Component Here */}
+                        <h1>{productItem.name}</h1>
+                        <>{productItem.descriptionHTML}</>
+                        <span>{productItem.displayName}</span>
+                        <ul>
+                            {productItem.options.values.map(
+                                (p)=> <li key= {p.label}>{p.label} :  
+                                {new Intl.NumberFormat('en-US', { style: 'currency', currency: `${p.price.currencyCode}` }).format(p.price.value)}</li>
+                                )}
+                        </ul>
+                        </> : <h2>Sorry, that product doesn't exist.</h2>}
 
             </Layout>
         )
