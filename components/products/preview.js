f00c93bd49e8c560852f24b5730ddb68bb3b3063
LeeGee64 <writerlelandgill@gmail.com>
2022-12-06 21:54:59 -0500

    dynamic link with preview component

--- components/products/preview.js
+++ components/products/preview.js
@@ -1,12 +1,17 @@
 import Image from 'next/image'
-// import Link from 'next/link'
+import Link from 'next/link'
 
 export default function Preview({productItem}) {
     return (
+    <Link href= {`/products/${productItem.slug}`}>
     <div id={productItem.name}>
         <Image alt={productItem.name} src={productItem.images[0].url} placeholder= "/product-img-placeholder.svg" width={100} height={100}/>
         <span>{productItem.name}</span>
-        {/* <span>{productItem.options.values[0].price}</span> */}
+        <span>From: 
+            {new Intl.NumberFormat('en-US', 
+            { style: 'currency', currency: `${productItem.options.values[0].price.currencyCode}` })
+            .format(productItem.options.values[0].price.value)}</span>
     </div>
+    </Link>
     );
 }
