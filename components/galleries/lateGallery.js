e2bcf532faca13d10d1b40e63e31e8e345ad214f
LeeGee64 <writerlelandgill@gmail.com>
2022-12-19 21:16:49 -0500

    testing gallery component

--- components/galleries/lateGallery.js
+++ components/galleries/lateGallery.js
@@ -1,8 +1,30 @@
 import Preview from "../products/preview";
+import Image from 'next/image'
 
-export default function LateGallery(lateArray){
+
+export default function LateGallery({lateArray}){
+
+    const leadProduct = lateArray[0]; 
+    const followProduct = lateArray.slice(1);
 
     return (
-        <></>
+            <div class="w-full h-80 grid grid-cols-2">
+                <div class="relative"><Image 
+                                        alt={leadProduct.name} 
+                                        src= {leadProduct.images[0].url} 
+                                        fill
+                                        objectFit="cover"
+                                        />
+                                        </div>
+                
+                <div class= "grid grid-rows-2 grid-cols-2">
+                {followProduct.map((p)=> (<div class="relative" key={p.id}>
+                                            <Image alt={p.name} 
+                                                src= {p.images[0].url} 
+                                                fill
+                                                objectFit= "cover"
+                                                 /></div>))}
+                </div>
+            </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout from '/components/layouts/layout'
 import Preview from '/components/products/preview'
 import useSWR from 'swr';
 import Link from 'next/link'
+import LateGallery from '../components/galleries/lateGallery';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -24,26 +25,28 @@ const { data, error } = useSWR(apiUrl, fetcher);
 
   return (
       <Layout>
+        <section id="latest">
+          <h2>Latest</h2>
+          {/* <ul class="flex flex-row">
+            {getLatest(prodArray,5)
+            .map((p)=> (<li key={p.id}><Preview productItem= {p}/></li>))}
+          </ul>
+          <br></br> */}
+          <LateGallery lateArray={getLatest(prodArray,5)}/>
+        </section>
+        <section  id="category">
+          <Link href='/categories'><h2>Categories</h2></Link>
+          <ul>
+            {catArray.map((p)=> (<li key={p}><Link href= {`/categories/${p}`}>{p.toUpperCase()}</Link></li>))}
+          </ul>
+        </section>
         <section id="intro">
           <Image alt="the bazaar" src="/bazaar.jpeg" placeholder="/product-img-placeholder.svg" width={500} height={500} priority/>
           <p>A one-stop shop for all of your magical needs! 
             All of our products our ethically sourced and sold fair trade, so you can 
             practice worry-free!</p>
         </section>
-        <section id="latest">
-          <h2>Latest</h2>
-          <ul class="flex flex-row first:rotate-180">
-            {getLatest(prodArray,5)
-            .map((p)=> (<li key={p.id}><Preview productItem= {p}/></li>))}
-          </ul>
-        </section>
-        <section id="category">
-          <Link href='/categories'><h2>Categories</h2></Link>
-          <ul>
-            {catArray.map((p)=> (<li key={p}><Link href= {`/categories/${p}`}>{p.toUpperCase()}</Link></li>))}
-          </ul>
-        </section>
-        <section id="whatis">
+        <section  id="whatis">
           <h2>What is this Place?</h2>
           <p>Short answer: A nexus for magic and wonder.</p>
           <p>Longer answer: alaffjosiosdiohsdfhiosdfhiofhioasdfio</p>
