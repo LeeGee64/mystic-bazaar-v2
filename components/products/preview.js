8e55f610567ac3d4aaadc6f4b22794d75c099c26
LeeGee64 <writerlelandgill@gmail.com>
2022-12-21 02:02:27 -0500

    struggling with pricing

--- components/products/preview.js
+++ components/products/preview.js
@@ -4,16 +4,6 @@ import { getPrice } from '/pages/api/productOperations'
 
 export default function Preview({productItem}) {
     return (
-    // <Link href= {`/products/${productItem.slug}`}>
-    // <div id={productItem.name}>
-    //     <Image alt={productItem.name} src={productItem.images[0].url} placeholder= "/product-img-placeholder.svg" width={100} height={100}/>
-    //     <span>{productItem.name}</span>
-    //     <span>From: {getPrice(productItem.options.values[0].price.currencyCode, 
-    //                             productItem.options.values[0].price.value)}</span>
-    // </div>
-    // </Link>
-    
-    // <Link class="max-h-full" href= {`/products/${productItem.slug}`}></Link>
    
     <div class="relative h-full" key={productItem.id} id={productItem.name}>
      <Link href= {`/products/${productItem.slug}`}>
--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,6 +1,7 @@
 import Layout from '/components/layouts/layout'
 import { getPrice } from '../api/productOperations'
 import { useRouter } from 'next/router'
+import React, { useState } from 'react';
 import ProductGallery from '/components/galleries/prodGallery'
 import useSWR from 'swr';
 
@@ -14,6 +15,12 @@ export default function Product() {
     
     const { data, error } = useSWR({product} ? apiUrl : null,fetcher);
     
+    const [calcedPrice,setCalcPrice] = React.useState(0);
+    const [selectPrice,setPrice] = React.useState(0);
+    const [selectQuant,setQuantity] = React.useState(1);
+
+    let priceCalc = 0;
+
         //Handle the error state
         if (error) return <div>Failed to load</div>;
         //Handle the loading state
@@ -23,24 +30,54 @@ export default function Product() {
         const prodArray = productObject.products;
 
         const productItem = prodArray.find((p) => p.slug === product);
-       
+
+        function calcPrice(e){
+            const {value} = e.currentTarget;  
+            setQuantity(value);    
+            priceCalc = selectQuant * selectPrice;
+            setCalcPrice(priceCalc);
+        }
+
+        function calcPrice2(price){
+            setPrice(price);
+            priceCalc = selectQuant * selectPrice;
+            setCalcPrice(priceCalc);
+        }
 
         return (
             <Layout> {productItem ? <div class="relative grid grid-cols-2 h-screen">
                      
-                        <ProductGallery product= {productItem} />
+                <ProductGallery product= {productItem} />
 
-                        <div class="relative">
-                            <h1>{productItem.name}</h1>
-                            <p>{productItem.description}</p>
-                            <span>{productItem.displayName}</span>
-                            <ul>
-                                {productItem.options.values.map(
-                                    (p)=> <li key= {p.label}>{p.label} :  
-                                    {getPrice(p.price.currencyCode, p.price.value)}</li>)}
-                            </ul>
-                        </div>
-                        </div> : <h2>Sorry, that product doesn't exist.</h2>}
+                <div class="relative">
+                    <h1>{productItem.name}</h1>
+                    <p>{productItem.description}</p>
+                    <span>{productItem.displayName}</span>
+                    <ul class="flex flex-row">
+                        {productItem.options.values.map(
+                            (p)=> <li class="p-[1rem] flex flex-col" 
+                                    key= {p.label}
+                                    onClick= {()=> {calcPrice2(p.price.value)}}
+                                    >
+                                {p.label} 
+                                <br/>
+                                {getPrice(p.price.currencyCode, p.price.value)}
+                                </li>)}
+                    </ul>
+                    <form onSubmit={e => e.preventDefault()}>
+                        <p>Quantity</p>
+                        <input id="priceCalc" 
+                            name="priceCalc"
+                            type="number"
+                            min="1"
+                            max="5"
+                            value={selectQuant}
+                            onChange= {e => {calcPrice(e)}}
+                            ></input>
+                            <p>{getPrice("USD",calcedPrice)}</p>
+                    </form>
+                </div>
+                </div> : <h2>Sorry, that product doesn't exist.</h2>}
 
             </Layout>
         )
