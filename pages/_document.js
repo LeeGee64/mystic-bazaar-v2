83832067ba3f4ec203168ed3d01a0276519b0f8b
LeeGee64 <writerlelandgill@gmail.com>
2023-02-22 16:52:00 -0500

    assistant font added

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ class MyDocument extends Document {
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;700&display=swap" rel="stylesheet"></link>
         </Head>
-        <body>
+        <body className="font-assistant">
           <Main />
           <NextScript />
         </body>
